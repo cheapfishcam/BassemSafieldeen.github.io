140dd1f4cb8cd260c0f658b3ce5477e7e0df938e
BassemSafieldeen <bassemsafieldeen@gmail.com>
2018-08-01 14:24:29 +0200

    attempting to call 3 people

--- js/script.js
+++ js/script.js
@@ -12,12 +12,21 @@ var config = {
 var database = firebase.database().ref();
 var yourVideo = document.getElementById("yourVideo");
 var friendsVideo = document.getElementById("friendsVideo");
+var otherfriendsVideo = document.getElementById("otherfriendsVideo");
 var yourId = Math.floor(Math.random()*1000000000);
+var flag1 = 1;
+var flag2 = 2;
+//var yourId = 1;
+//var friendsId = 2;
+//var otherfriendsId = 3;
 //Create an account on Viagenie (http://numb.viagenie.ca/), and replace {'urls': 'turn:numb.viagenie.ca','credential': '13111994','username': 'bassemsafieldeen@gmail.com'} with the information from your account
 var servers = {'iceServers': [{'urls': 'stun:stun.services.mozilla.com'}, {'urls': 'stun:stun.l.google.com:19302'}, {'urls': 'turn:numb.viagenie.ca','credential': '13111994','username': 'bassemsafieldeen@gmail.com'}]};
-var pc = new RTCPeerConnection(servers);
-pc.onicecandidate = (event => event.candidate?sendMessage(yourId, JSON.stringify({'ice': event.candidate})):console.log("Sent All Ice") );
-pc.onaddstream = (event => friendsVideo.srcObject = event.stream);
+var pc1 = new RTCPeerConnection(servers);
+var pc2 = new RTCPeerConnection(servers);
+pc1.onicecandidate = (event => event.candidate?sendMessage(yourId, JSON.stringify({'ice': event.candidate})):console.log("Sent All Ice") );
+pc1.onaddstream = (event => friendsVideo.srcObject = event.stream);
+pc2.onicecandidate = (event => event.candidate?sendMessage(yourId, JSON.stringify({'ice': event.candidate})):console.log("Sent All Ice") );
+pc2.onaddstream = (event => otherfriendsVideo.srcObject = event.stream);
 
 function sendMessage(senderId, data) {
     var msg = database.push({ sender: senderId, message: data });
@@ -27,16 +36,29 @@ function sendMessage(senderId, data) {
 function readMessage(data) {
     var msg = JSON.parse(data.val().message);
     var sender = data.val().sender;
-    if (sender != yourId) {
+    if (sender != yourId && flag1 == 1 || sender == flag1) {
+        flag1 = sender;
         if (msg.ice != undefined)
-            pc.addIceCandidate(new RTCIceCandidate(msg.ice));
+            pc1.addIceCandidate(new RTCIceCandidate(msg.ice));
         else if (msg.sdp.type == "offer")
-            pc.setRemoteDescription(new RTCSessionDescription(msg.sdp))
-              .then(() => pc.createAnswer())
-              .then(answer => pc.setLocalDescription(answer))
-              .then(() => sendMessage(yourId, JSON.stringify({'sdp': pc.localDescription})));
+            pc1.setRemoteDescription(new RTCSessionDescription(msg.sdp))
+              .then(() => pc1.createAnswer())
+              .then(answer => pc1.setLocalDescription(answer))
+              .then(() => sendMessage(yourId, JSON.stringify({'sdp': pc1.localDescription})));
         else if (msg.sdp.type == "answer")
-            pc.setRemoteDescription(new RTCSessionDescription(msg.sdp));
+            pc1.setRemoteDescription(new RTCSessionDescription(msg.sdp));
+    }
+    if (sender != yourId && flag2 == 2 && sender != flag1 || sender == flag2) {
+        flag2 = sender;
+        if (msg.ice != undefined)
+            pc2.addIceCandidate(new RTCIceCandidate(msg.ice));
+        else if (msg.sdp.type == "offer")
+            pc2.setRemoteDescription(new RTCSessionDescription(msg.sdp))
+              .then(() => pc2.createAnswer())
+              .then(answer => pc2.setLocalDescription(answer))
+              .then(() => sendMessage(yourId, JSON.stringify({'sdp': pc2.localDescription})));
+        else if (msg.sdp.type == "answer")
+            pc2.setRemoteDescription(new RTCSessionDescription(msg.sdp));
     }
 };
 
@@ -45,11 +67,18 @@ database.on('child_added', readMessage);
 function showMyFace() {
   navigator.mediaDevices.getUserMedia({audio:true, video:true})
     .then(stream => yourVideo.srcObject = stream)
-    .then(stream => pc.addStream(stream));
+    .then(stream => pc1.addStream(stream));
+    .then(stream => pc2.addStream(stream));
 }
 
 function showFriendsFace() {
-  pc.createOffer()
-    .then(offer => pc.setLocalDescription(offer) )
-    .then(() => sendMessage(yourId, JSON.stringify({'sdp': pc.localDescription})) );
+  pc1.createOffer()
+    .then(offer => pc1.setLocalDescription(offer) )
+    .then(() => sendMessage(yourId, JSON.stringify({'sdp': pc1.localDescription})) );
+}
+
+function showOtherFriendsFace() {
+  pc2.createOffer()
+    .then(offer => pc2.setLocalDescription(offer) )
+    .then(() => sendMessage(yourId, JSON.stringify({'sdp': pc2.localDescription})) );
 }
