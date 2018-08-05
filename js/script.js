156f44bab35ae614480173443ef96e7860309692
BassemSafieldeen <bassemsafieldeen@gmail.com>
2018-08-05 03:35:09 +0200

    .

--- js/script.js
+++ js/script.js
@@ -213,9 +213,9 @@ database.on('child_added', readMessage);
 function showMyFace() {
   navigator.mediaDevices.getUserMedia({audio:true, video:true})
     .then(stream => yourVideo.srcObject = stream)
-    .then(stream => pc01.addStream(stream))
+    .then(stream => pc12.addStream(stream))
     .then(stream => pc02.addStream(stream))
-    .then(stream => pc12.addStream(stream));
+    .then(stream => pc01.addStream(stream));
 }
 
 function showFriendsFace() {
